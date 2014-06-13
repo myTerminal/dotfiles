4cf3948ac8c9aa379dc3e53d9c09aaac440950de
Ismail Ansari <ismail.ansari@citiustech.com>
2014-06-13 17:55:01 +0530

    Updated packages

--- Standard/.emacs.d/elpa/e2wm-20130909.2314/e2wm-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "e2wm" "20130909.2314" "simple window manager for emacs"
-  '((window-layout "1.2")))
---
+++ Standard/.emacs.d/elpa/e2wm-20140612.1804/e2wm-pkg.el
@@ -0,0 +1,4 @@
+(define-package "e2wm" "20140612.1804" "simple window manager for emacs"
+  '((window-layout "1.2"))
+  :keywords
+  '("tools" "window manager"))
