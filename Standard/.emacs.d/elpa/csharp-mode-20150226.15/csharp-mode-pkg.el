40eb42bd791e8bea0e78c2327a0e7a1f5416dcb7
Ismail Ansari <ismail.ansari@citiustech.com>
2015-03-20 18:11:55 +0530

    Updated packages

--- Standard/.emacs.d/elpa/csharp-mode-20150226.15/csharp-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "csharp-mode" "20150226.15" "C# mode derived mode" (quote nil))
--- Standard/.emacs.d/elpa/rainbow-delimiters-20141221.925/rainbow-delimiters-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rainbow-delimiters" "20141221.925" "Highlight brackets according to their depth" 'nil :url "https://github.com/Fanael/rainbow-delimiters" :keywords '("faces" "convenience" "lisp" "tools"))
