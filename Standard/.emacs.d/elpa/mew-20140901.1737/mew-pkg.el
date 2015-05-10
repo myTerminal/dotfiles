538eeaf58c3732b8bb1f375621448f86f4e9c096
myTerminal <team.terminal@gmail.com>
2015-05-10 20:17:23 +0530

    update packages

--- Standard/.emacs.d/elpa/mew-20140901.1737/mew-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "mew" "20140901.1737" "Messaging in the Emacs World" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ Standard/.emacs.d/elpa/mew-20150507.1717/mew-pkg.el
@@ -0,0 +1,4 @@
+(define-package "mew" "20150507.1717" "Messaging in the Emacs World" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
