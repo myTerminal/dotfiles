97cbc5ee9d0eaa03cf9ce8ffb043bc51abaedcb0
Ismail Ansari <ismail.ansari@citiustech.com>
2015-06-11 11:27:52 +0530

    add missing file reference for file-associations.el in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 (load-file "~/.emacs.d/myTerminal/user.el")
 (load-file "~/.emacs.d/myTerminal/repositories.el")
 (load-file "~/.emacs.d/myTerminal/packages.el")
+(load-file "~/.emacs.d/myTerminal/file-associations.el")
 (load-file "~/.emacs.d/myTerminal/interface.el")
 (load-file "~/.emacs.d/myTerminal/jabber-configuration.el")
 (load-file "~/.emacs.d/myTerminal/misc.el")
