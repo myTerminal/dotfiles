78563eafcc8fb134c4872db26c7444c3353c03f4
myTerminal <team.terminal@gmail.com>
2015-06-14 15:53:03 +0530

    remove repository elpa-old

--- .emacs.d/myTerminal/repositories.el
+++ .emacs.d/myTerminal/repositories.el
@@ -1,11 +1,6 @@
 ;Load package.el
 (require 'package)
 
-;Add old elpa to the list of repositories
-(add-to-list 'package-archives
-             '("elpa-old" . "http://tromey.com/elpa/")
-             t)
-
 ;Add melpa to list of repositories
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") 
