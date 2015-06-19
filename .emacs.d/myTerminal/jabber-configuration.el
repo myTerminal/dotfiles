224c98876d8d3733fe50b559055829dc3bb234ed
myTerminal <team.terminal@gmail.com>
2015-06-19 10:55:06 +0530

    remaster jabber notifications

--- .emacs.d/myTerminal/jabber-configuration.el
+++ .emacs.d/myTerminal/jabber-configuration.el
@@ -1,3 +1,4 @@
+;Configure jabber accounts
 (setq jabber-account-list
       '(("ismaila@citiusim.mumbai1.corp.citiustech.com" 
 	 (:network-server . "citiusim.mumbai1.corp.citiustech.com"))
@@ -5,6 +6,7 @@
 	 (:network-server . "talk.google.com")
 	 (:connection-type . ssl))))
 
+;Disable jabber alerts
 (setq jabber-alert-presence-message-function 
       (lambda (who 
                oldstatus 
@@ -12,21 +14,43 @@
                statustext) 
         nil))
 
+;Wrapper function for jabber notifications
 (defun myTerminal/custom-notification-for-jabber
+    nil)
+
+;Jabber notification timer
+(defvar myTerminal/notification-timer
   nil)
 
+;Set jabber alert message hook
 (add-hook 'jabber-alert-message-hooks
           'myTerminal/custom-notification-for-jabber)
 
+(defun myTerminal/show-jabber-notification ()
+  "Start timer for visual notification"
+  (myTerminal/dismiss-jabber-notification)
+  (setq myTerminal/notification-timer
+        (run-at-time 1
+                     1
+                     'myTerminal/flash-screen)))
+
+(defun myTerminal/dismiss-jabber-notification ()
+  "Dismiss visual notification"
+  (interactive)
+  (when (not (null myTerminal/notification-timer))
+    (cancel-timer myTerminal/notification-timer)))
+
 (defun myTerminal/flash-screen ()
+  "Flash screen once"
   (set-face-background 'fringe "red")
-  (run-at-time 0.25
+  (run-at-time 0.5
                nil
                (lambda (x)
                  (set-face-background 'fringe nil))
                t))
 
 (defun myTerminal/switch-to-visual-notification-for-jabber ()
+  "Switch to visual notification"
   (setq visible-bell
         nil)
   (fset 'myTerminal/custom-notification-for-jabber
@@ -34,9 +58,10 @@
                  buffer
                  text
                  propsed-alert)
-          (myTerminal/flash-screen))))
+          (myTerminal/show-jabber-notification))))
 
 (defun myTerminal/switch-to-taskbar-notification-for-jabber ()
+  "Switch to taskbar notification"
   (setq visible-bell
         t)
   (fset 'myTerminal/custom-notification-for-jabber
