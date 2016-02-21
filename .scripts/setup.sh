897a0ac343a35718a292bf4eb8a66ec0b3111ba3
myTerminal <team.terminal@gmail.com>
2016-02-21 22:38:09 +0530

    update setup.sh

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copy i3status.conf
+sudo mv .scripts/resources/i3status.conf /etc/i3status.conf
+
+# Set up group for docker
 sudo groupadd docker
 sudo gpasswd -a ${USERNAME} docker
 sudo service docker restart
