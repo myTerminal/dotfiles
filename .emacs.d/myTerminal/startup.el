f642f928554623305eb4b7563691b85dfa5532f8
myTerminal <team.terminal@gmail.com>
2016-06-02 21:13:45 +0530

    create functions for basic and full modes

--- .emacs.d/myTerminal/startup.el
+++ .emacs.d/myTerminal/startup.el
@@ -1,3 +1,15 @@
+(defun myTerminal/start-basic-mode ()
+  (myTerminal/set-user-preferences)
+  (myTerminal/set-up-repositories)
+  (if (y-or-n-p "Install pending packages?")
+      (myTerminal/set-up-package-lists))
+  (myTerminal/configure-basic-mode-packages)
+  (myTerminal/set-file-associations)
+  (myTerminal/configure-interface)
+  (myTerminal/define-personal-functions)
+  (myTerminal/define-basic-mode-keyboard-bindings)
+  (myTerminal/apply-temporary-configurations))
+
 (defun myTerminal/prompt-for-full-mode ()
   (interactive)
   (if (y-or-n-p "Not running in full-mode. Do you want to progress to full-mode?")
@@ -7,17 +19,9 @@
   (interactive)
   (myTerminal/configure-full-mode-packages)
   (myTerminal/configure-jabber)
+  (myTerminal/define-full-mode-keyboard-bindings)
   (message "Progressed to full-mode"))
 
-(myTerminal/set-user-preferences)
-(myTerminal/set-up-repositories)
-(if (y-or-n-p "Install pending packages?")
-    (myTerminal/set-up-package-lists))
-(myTerminal/configure-basic-mode-packages)
-(myTerminal/configure-full-mode-packages) ;To be removed
-(myTerminal/set-file-associations)
-(myTerminal/configure-interface)
-(myTerminal/define-personal-functions)
-(myTerminal/define-basic-mode-keyboard-bindings)
-(myTerminal/define-full-mode-keyboard-bindings) ;To be removed
-(myTerminal/apply-temporary-configurations)
+;Start in basic-mode, progress to full mode
+(myTerminal/start-basic-mode)
+(myTerminal/progress-to-full-mode)
