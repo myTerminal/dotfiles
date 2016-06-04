9c550856f5d4a6af86154de3b3d0e11912e5b584
myTerminal <team.terminal@gmail.com>
2016-06-04 08:49:25 +0530

    add key-binding to progress to full mode

--- .emacs.d/myTerminal/key-bindings.el
+++ .emacs.d/myTerminal/key-bindings.el
@@ -20,6 +20,7 @@
     ("C-S-<right>" buf-move-right nil)
     ("<f5>" myTerminal/reload-current-file nil)
     ("C-x C-/" halal-mode nil)
+    ("C-x C-\\" myTerminal/progress-to-full-mode)
     ("C-M-)" transparency-increase t)
     ("C-M-(" transparency-decrease t)
     ("C-c M-x" smex t)
