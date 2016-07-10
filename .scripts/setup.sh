20049c76ee6832dcd6635d0b1b36a632d853bb9a
myTerminal <team.terminal@gmail.com>
2016-07-10 21:29:41 +0530

    add script to install important packages

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install packages
+sudo dnf install VirtualBox audacity vlc nodejs npm emacs git udisks youtube-dl gimp clementine libreoffice gparted docker.io i3 i3status i3lock feh pcmanfm shotwell levein-inconsolata-fonts openshot gource xbmc goldendict vokoscreen calibre terminology
+
 # Copy i3status.conf
 sudo mv .scripts/resources/i3status.conf /etc/i3status.conf
 
