e3c1316dfb746db435035f5531cebdc1790bf28c
myTerminal <team.terminal@gmail.com>
2016-11-05 15:19:41 +0530

    update package list

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install packages
-sudo dnf install VirtualBox audacity vlc nodejs npm emacs git udisks youtube-dl gimp clementine libreoffice gparted docker.io i3 i3status i3lock feh pcmanfm shotwell levein-inconsolata-fonts openshot gource xbmc goldendict vokoscreen calibre terminology
+sudo dnf install VirtualBox audacity vlc nodejs npm emacs git udisks youtube-dl gimp clementine libreoffice gparted docker.io i3 i3status i3lock feh pcmanfm shotwell openshot gource xbmc goldendict vokoscreen calibre terminology easytag key-mon levein-inconsolata-fonts liberation-mono-fonts google-droid-sans-mono-fonts google-roboto-mono-fonts
 
 # Copy i3status.conf
 sudo mv .scripts/resources/i3status.conf /etc/i3status.conf
