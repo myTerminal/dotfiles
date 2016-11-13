acff1729110d33997472102353447191d9ad039b
myTerminal <team.terminal@gmail.com>
2016-11-13 09:07:23 +0530

    update setup scripts

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 
+# Update installed packages
+sudo dnf update
+
 # Install packages
-sudo dnf install VirtualBox audacity vlc nodejs npm emacs git udisks youtube-dl gimp clementine libreoffice gparted docker.io i3 i3status i3lock feh pcmanfm shotwell openshot gource xbmc goldendict vokoscreen calibre terminology easytag key-mon levein-inconsolata-fonts liberation-mono-fonts google-droid-sans-mono-fonts google-roboto-mono-fonts
+sudo dnf install \
+     google-chrome \
+     i3 \
+     i3status \
+     i3lock \
+     terminology \
+     pcmanfm \
+     emacs \
+     git \
+     npm \
+     udisks \
+     dropbox \
+     gparted \
+     libreoffice \
+     VirtualBox \
+     audacity \
+     vlc \
+     gimp \
+     clementine \
+     docker \
+     gource \
+     feh \
+     shotwell \
+     openshot \
+     kodi \
+     goldendict \
+     vokoscreen \
+     calibre \
+     easytag \
+     key-mon \
+     youtube-dl \
+     levien-inconsolata-fonts \
+     liberation-mono-fonts \
+     google-droid-sans-mono-fonts \
+     google-roboto-mono-fonts
 
 # Copy i3status.conf
-sudo mv .scripts/resources/i3status.conf /etc/i3status.conf
+sudo cp .scripts/resources/i3status.conf /etc/i3status.conf
 
 # Set up group for docker
 sudo groupadd docker
 sudo gpasswd -a ${USERNAME} docker
 sudo service docker restart
-
