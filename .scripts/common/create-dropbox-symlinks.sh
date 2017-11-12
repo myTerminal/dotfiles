5ececc5b17041493b49c55511ea5f29d7ce7adb9
myTerminal <team.terminal@gmail.com>
2017-11-12 17:38:43 -0600

    add script to create dropbox symlinks

---
+++ .scripts/common/create-dropbox-symlinks.sh
@@ -0,0 +1,4 @@
+ln -s ~/Dropbox/Workstation/.private-keys.sh ~/.private-keys.sh
+ln -s ~/Dropbox/eDocket ~/Documents/eDocket
+ln -s ~/Dropbox/Documents ~/Documents/synced-documents
+ln -s ~/Dropbox/Islamic-Inventory ~/Documents/Islamic-Inventory
