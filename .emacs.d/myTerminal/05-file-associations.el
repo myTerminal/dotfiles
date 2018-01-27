4322c16a76948bbbec910ea3c91e298f08261863
myTerminal <team.terminal@gmail.com>
2018-01-27 13:46:30 -0600

    associate .jsx files with web-mode for emacs

--- .emacs.d/myTerminal/05-file-associations.el
+++ .emacs.d/myTerminal/05-file-associations.el
@@ -3,6 +3,8 @@
                '("\\.htm\\'" . web-mode))
   (add-to-list 'auto-mode-alist
                '("\\.html\\'" . web-mode))
+  (add-to-list 'auto-mode-alist
+               '("\\.jsx\\'" . web-mode))
 
   (add-to-list 'auto-mode-alist
                '("\\.js\\'" . js2-mode))
