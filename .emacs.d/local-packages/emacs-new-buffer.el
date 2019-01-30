1dde1ad9d6a938794bf0d1f2b6a721281bc28a81
myTerminal <team.terminal@gmail.com>
2019-01-30 00:04:38 -0600

    update local package emacs-new-buffer

--- .emacs.d/local-packages/emacs-new-buffer.el
+++ .emacs.d/local-packages/emacs-new-buffer.el
@@ -4,7 +4,7 @@
 
 ;; Author: Mohammed Ismail Ansari <team.terminal@gmail.com>
 ;; Keywords: convenience
-;; Package-Version: 20171026.1922
+;; Package-Version: 20190128.1912
 ;; Maintainer: Mohammed Ismail Ansari <team.terminal@gmail.com>
 ;; Created: 2017/10/26
 ;; Package-Requires: ((emacs "24"))
@@ -64,21 +64,19 @@
   "Creates a new buffer with timestamp as the name"
   (interactive)
   (let ((new-buffer-name (emacs-new-buffer--get-buffer-name)))
-    (find-file (concatenate 'string
-                            "~/"
-                            "temp-"
-                            new-buffer-name))))
+    (find-file (concat "~/"
+                       "temp-"
+                       new-buffer-name))))
 
 (defun emacs-new-buffer-as (extension)
   "Creates a new buffer with timestamp as the name and extension as specified"
   (interactive "sEnter a file extension for the new buffer: ")
   (let ((new-buffer-name (emacs-new-buffer--get-buffer-name)))
-    (find-file (concatenate 'string
-                            "~/"
-                            "temp-"
-                            new-buffer-name
-                            "."
-                            extension))))
+    (find-file (concat "~/"
+                       "temp-"
+                       new-buffer-name
+                       "."
+                       extension))))
 
 (defun emacs-new-buffer--get-buffer-name ()
   (format-time-string "%Y%m%d-%H%M%S"))
