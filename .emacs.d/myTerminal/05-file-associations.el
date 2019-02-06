45bfa35f2ce3e937c95b44bd92673d35344b8fbb
myTerminal <team.terminal@gmail.com>
2019-02-05 21:04:04 -0600

    set file association for TypeScript in emacs

--- .emacs.d/myTerminal/05-file-associations.el
+++ .emacs.d/myTerminal/05-file-associations.el
@@ -8,6 +8,8 @@
 
   (add-to-list 'auto-mode-alist
                '("\\.js\\'" . js2-mode))
+  (add-to-list 'auto-mode-alist
+               '("\\.ts\\'" . typescript-mode))
 
   (add-to-list 'auto-mode-alist
                '("\\.scss\\'" . scss-mode))
