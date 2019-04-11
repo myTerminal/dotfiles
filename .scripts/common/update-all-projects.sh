4bb3bdd3f36ba23e39a5a563cb5ce9e6521b1e28
myTerminal <team.terminal@gmail.com>
2019-04-11 00:20:31 -0500

    upgrade script to update GitHub projects

--- .scripts/common/update-all-projects.sh
+++ .scripts/common/update-all-projects.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+count=0
+
 cd $1
+
 for i in `ls $1`;
 do
     cd $i
+
     echo ""
     echo + $i
+
     git pull
+
+    ((count++))
+
     cd ..;
 done
+
+echo ""
+echo "Done updating" $count "projects"
 echo ""
