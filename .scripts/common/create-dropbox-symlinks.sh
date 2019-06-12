f6c92ab6f27644b8a39b7679cac9ac05074c43b0
Mohammed Ismail Ansari <team.terminal@gmail.com>
2019-06-12 10:02:43 -0500

    remove reference to deleted folder on Dropbox

--- .scripts/common/create-dropbox-symlinks.sh
+++ .scripts/common/create-dropbox-symlinks.sh
@@ -1,4 +1,3 @@
 ln -s ~/Dropbox/Workstation/.private-keys.sh ~/.private-keys.sh
 ln -s ~/Dropbox/eDocket ~/Documents/eDocket
-ln -s ~/Dropbox/Documents ~/Documents/synced-documents
 ln -s ~/Dropbox/Islamic-Inventory ~/Documents/Islamic-Inventory
