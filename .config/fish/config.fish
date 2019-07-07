5466ca73e499b58d3181286b6169e0a2358a8753
myTerminal <team.terminal@gmail.com>
2019-07-07 00:16:53 -0500

    add initial fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,22 @@
-# To be set up
+# Expose shell scripts as commands
+# (Already inherited from bash config)
+
+# Load configs for the current OS
+if test (uname) = "Linux"
+    . ~/.config/fish/config-fedora.fish
+else if test (uname) = "Darwin"
+    . ~/.config/fish/config-mac.fish
+end
+
+# Load shell configs
+. ~/.shell/config
+
+# Set up common shell aliases
+. ~/.shell/aliases/common
+
+# Load aliases for the current OS
+if test (uname) = "Linux"
+    . ~/.shell/aliases/fedora
+else if test (uname) = "Darwin"
+    . ~/.shell/aliases/mac
+end
