568ff651f10649dc62597e8ff0a5f7e5a53a2e5b
myTerminal <team.terminal@gmail.com>
2019-10-24 23:12:12 -0500

    rename emacs configuration-files to remove leading sequence-numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,20 +1,20 @@
 (defvar myTerminal/invokation-time
   (current-time))
 
-(load-file "~/.emacs.d/myTerminal/00-system.el")
-(load-file "~/.emacs.d/myTerminal/01-user.el")
-(load-file "~/.emacs.d/myTerminal/02-repositories.el")
-(load-file "~/.emacs.d/myTerminal/03-packages.el")
-(load-file "~/.emacs.d/myTerminal/04-jabber-configuration.el")
-(load-file "~/.emacs.d/myTerminal/05-file-associations.el")
-(load-file "~/.emacs.d/myTerminal/06-interface.el")
-(load-file "~/.emacs.d/myTerminal/07-misc.el")
-(load-file "~/.emacs.d/myTerminal/08-key-bindings.el")
+(load-file "~/.emacs.d/myTerminal/system.el")
+(load-file "~/.emacs.d/myTerminal/user.el")
+(load-file "~/.emacs.d/myTerminal/repositories.el")
+(load-file "~/.emacs.d/myTerminal/packages.el")
+(load-file "~/.emacs.d/myTerminal/jabber-configuration.el")
+(load-file "~/.emacs.d/myTerminal/file-associations.el")
+(load-file "~/.emacs.d/myTerminal/interface.el")
+(load-file "~/.emacs.d/myTerminal/misc.el")
+(load-file "~/.emacs.d/myTerminal/key-bindings.el")
 
-(load-file "~/.emacs.d/myTerminal/09-default-local-configs.el")
+(load-file "~/.emacs.d/myTerminal/default-local-configs.el")
 (if (file-exists-p "~/.emacs.d/local-configs.el")
     (load-file "~/.emacs.d/local-configs.el"))
 
-(load-file "~/.emacs.d/myTerminal/10-startup.el")
+(load-file "~/.emacs.d/myTerminal/startup.el")
 
 (load-file "~/.emacs.d/myTerminal/_zone-quotes-masseffect.el")
