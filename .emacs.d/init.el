41ede9f028b597a3c2d88ce310f2ac8abde35b5f
myTerminal <team.terminal@gmail.com>
2019-11-02 00:01:49 -0500

    skip prompt to select a configuration for Emacs when there's only one

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,14 @@
 ;;Prompt to select a configuration to start Emacs with
-(let ((choices (directory-files "~/.emacs.d/configs"
-                                nil
-                                "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)")))
+(let ((available-configs (directory-files "~/.emacs.d/configs"
+                                          nil
+                                          "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))
+      (selected-config nil))
+  (cond ((= (length available-configs) 1) (setq selected-config
+                                                (car available-configs)))
+	(t (setq selected-config
+		 (ido-completing-read "Select a configuration:"
+                                      available-configs))))
   (load (expand-file-name (concat "configs/"
-                                  (ido-completing-read "Select a configuration:" choices)
-                                  "/.emacs.d/init")
-                          (file-name-directory load-file-name))))
+				  selected-config
+				  "/.emacs.d/init")
+			  (file-name-directory load-file-name))))
