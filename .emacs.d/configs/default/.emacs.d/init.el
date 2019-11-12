27776b7116e50a6326e09c002dabb12410aedd27
myTerminal <team.terminal@gmail.com>
2019-11-11 20:53:41 -0600

    rename config files for Emacs

--- .emacs.d/configs/default/.emacs.d/init.el
+++ .emacs.d/configs/default/.emacs.d/init.el
@@ -6,16 +6,18 @@
 (load (expand-file-name "myTerminal/core"
 			mt/config-root))
 
+;;Load config files sequentially
 (mt/load-file "config-system")
-(mt/load-file "config-user")
-(mt/load-file "config-local-configs")
+(mt/load-file "config-user-variables")
+(mt/load-file "config-workstation-variables")
 (mt/load-file "config-repositories")
 (mt/load-file "config-packages")
-(mt/load-file "config-jabber-configuration")
+(mt/load-file "config-jabber")
 (mt/load-file "config-file-associations")
 (mt/load-file "config-interface")
 (mt/load-file "config-key-bindings")
 (mt/load-file "_zone-quotes-masseffect")
 
+;;Finish startup
 (mt/print-welcome-message)
 (mt/load-personalization-variables)
