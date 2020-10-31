4f4f94878df0e6d9ccb8d96a329f70aabf39291e
myTerminal <team.terminal@gmail.com>
2020-10-31 17:45:45 -0500

    replace power mode related aliases with a script

--- .config/fish/aliases-linux.fish
+++ .config/fish/aliases-linux.fish
@@ -2,10 +2,3 @@ alias mt-desktop1="startx ~/.xinitrc"
 alias mt-desktop2="startx ~/.xinitrc secondary"
 
 alias keymon-start="key-mon --key-timeout=1.5 --no-press-fadeout=2 --old-keys=5"
-
-alias mt-power-prevent-sleep="systemctl mask sleep.target suspend.target"
-alias mt-power-allow-sleep="systemctl unmask sleep.target suspend.target"
-alias mt-power-shutdown="systemctl poweroff"
-alias mt-power-reboot="systemctl reboot"
-alias mt-power-suspend="systemctl suspend"
-alias mt-power-hibernate="systemctl hibernate"
