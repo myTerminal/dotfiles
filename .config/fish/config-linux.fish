e982a4d5d8590e6eb8590c57f9617a45bf9ad6f6
myTerminal <team.terminal@gmail.com>
2020-11-26 21:37:48 -0600

    add aliases for optimus-manager

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,4 +1,7 @@
 # Set up aliases
 alias mt-desktop1="startx ~/.xinitrc"
 alias mt-desktop2="startx ~/.xinitrc secondary"
+alias mt-gpu-i="optimus-manager --switch intel"
+alias mt-gpu-o="optimus-manager --switch hybrid"
+alias mt-gpu-p="optimus-manager --switch nvidia"
 alias keymon-start="key-mon --key-timeout=1.5 --no-press-fadeout=2 --old-keys=5"
