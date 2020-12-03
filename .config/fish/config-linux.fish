b50b1125e54271b9ea9a2f60fad11b9b0d53bc7a
myTerminal <team.terminal@gmail.com>
2020-12-02 19:31:35 -0600

    create a script for handling startx

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -2,6 +2,4 @@
 add-to-path ~/.scripts/linux
 
 # Set up aliases
-alias mt-desktop1="startx ~/.xinitrc"
-alias mt-desktop2="startx ~/.xinitrc secondary"
 alias keymon-start="key-mon --key-timeout=1.5 --no-press-fadeout=2 --old-keys=5"
