9ba744639deab78c34c6ff1c45994bc8ec995a05
myTerminal <team.terminal@gmail.com>
2021-04-30 21:11:55 -0500

    implement loading of distro-specific utility scripts

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,5 +1,12 @@
-# Update PATH for custom shell scripts for Linux
+# Add generic Linux scripts
 add-to-path ~/.scripts/linux
+
+# Add rest of the distro specific scripts
+if test (command -v pacman)
+    add-to-path ~/.scripts/linux/arch
+else if test (command -v apt)
+    add-to-path ~/.scripts/linux/debian
+end
 
 # Set up aliases
 alias keymon-start="key-mon --key-timeout=1.5 --no-press-fadeout=2 --old-keys=5"
