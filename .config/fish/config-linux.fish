72b4be5cd62e7b772ac3c8b25bf0f106823e8c58
myTerminal <team.terminal@gmail.com>
2021-09-19 11:36:17 -0500

    add xsel for clipboard in Linux

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -11,4 +11,6 @@ else if test (command -v xbps-query)
 end
 
 # Set up aliases
+alias xcopy="xsel --clipboard --input"
+alias xpaste="xsel --clipboard --output"
 alias keymon-start="key-mon --key-timeout=1.5 --no-press-fadeout=2 --old-keys=5"
