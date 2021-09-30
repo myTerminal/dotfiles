ecfcb77f2b3d8011e1631ce1c7416837a01a7532
myTerminal <team.terminal@gmail.com>
2021-09-30 00:01:37 -0500

    remove adding of distro-specific scripts to PATH for Linux

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,15 +1,6 @@
 # Add generic Linux scripts
 add-to-path ~/.scripts/linux
 
-# Add rest of the distro specific scripts
-if test (command -v pacman)
-    add-to-path ~/.scripts/linux/arch
-else if test (command -v apt)
-    add-to-path ~/.scripts/linux/debian
-else if test (command -v xbps-query)
-    add-to-path ~/.scripts/linux/void
-end
-
 # Set up aliases
 alias xcopy="xsel --clipboard --input"
 alias xpaste="xsel --clipboard --output"
