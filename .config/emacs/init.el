087960afc38ee3e4f0fc9af516129b079cd099c7
myTerminal <team.terminal@gmail.com>
2023-03-16 21:19:19 -0500

    simplify .config/emacs/init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,22 +1,17 @@
-;;Prompt to select a configuration to start Emacs with
-(let ((available-configs (if (file-directory-p "~/.config/emacs/configs")
-                             (directory-files "~/.config/emacs/configs"
-                                              nil
-                                              "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)")
-                           '()))
-      (selected-config nil))
-  (if (null available-configs)
-      (message "No configs found")
-    (progn
-      (cond ((= (length available-configs) 1) (setq selected-config
-                                                    (car available-configs)))
-	    (t (setq selected-config
-		     (ido-completing-read "Select a configuration:"
-                                          available-configs))))
-      (load (expand-file-name (concat "configs/"
-				      selected-config
-				      "/init")
-			      (file-name-directory load-file-name))))))
+;; Prompt to select a configuration to start Emacs with
+(let ((configs (if (file-directory-p "~/.config/emacs/configs")
+                   (directory-files "~/.config/emacs/configs"
+                                    nil
+                                    "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))))
+  (if (null configs)
+      (message "No configuration found!")
+    (load (expand-file-name (concat "configs/"
+				                    (cond ((cdr configs) (ido-completing-read
+                                                          "Select a configuration: "
+                                                          configs))
+	                                      (t (car configs)))
+				                    "/init")
+			                (file-name-directory load-file-name)))))
 
 ;; Load user-specific config
 (load (concat (file-name-directory user-init-file)
