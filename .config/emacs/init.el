07edaa06ed8a9d342b6647fb61b84dcf5986e673
myTerminal <team.terminal@gmail.com>
2023-03-16 21:32:51 -0500

    skip prompting for configs to load while launching with a file

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,19 +1,19 @@
-;; Prompt to select a configuration to start Emacs with
-(let ((configs (if (file-directory-p "~/.config/emacs/configs")
-                   (directory-files "~/.config/emacs/configs"
-                                    nil
-                                    "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))))
-  (if (null configs)
-      (message "No configuration found!")
-    (load (expand-file-name (concat "configs/"
-				                    (cond ((cdr configs) (ido-completing-read
-                                                          "Select a configuration: "
-                                                          configs))
-	                                      (t (car configs)))
-				                    "/init")
-			                (file-name-directory load-file-name)))))
+(if (or (null (cdr command-line-args))
+        (null (seq-filter #'file-exists-p (cdr command-line-args))))
+    (let ((configs (if (file-directory-p "~/.config/emacs/configs")
+                       (directory-files "~/.config/emacs/configs"
+                                        nil
+                                        "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))))
+      (if (null configs)
+          (message "No configuration found!")
+        (load (expand-file-name (concat "configs/"
+				                        (cond ((cdr configs) (ido-completing-read
+                                                              "Select a configuration: "
+                                                              configs))
+	                                          (t (car configs)))
+				                        "/init")
+			                    (file-name-directory load-file-name))))))
 
-;; Load user-specific config
 (load (concat (file-name-directory user-init-file)
               "user")
       t)
