d2cece145741e10d3a8cc64fb7a080577eb116a5
myTerminal <team.terminal@gmail.com>
2023-11-05 09:37:34 -0600

    fix xrandr-parser.lisp

--- .assets/parsers/xrandr-parser.lisp
+++ .assets/parsers/xrandr-parser.lisp
@@ -1,38 +1,23 @@
 #!/usr/bin/sbcl --script
 
-(defun string-to-list (input-string char)
-  "Converts a string containing NewLine characters into a list of strings."
-  (let* ((temp-chars '())
-         (items '()))
-    (labels ((to-string (chars)
-               (coerce (reverse chars) 'string))
-             (collect-item ()
-               (push (to-string temp-chars) items)
-               (setf temp-chars '())))
-      (mapc (lambda (c)
-              (cond ((eql c char) (collect-item))
-                    (t (push c temp-chars))))
-            (coerce input-string 'list))
-      (collect-item)
-      (reverse items))))
+(defun get-device-names (arguments state)
+  "Gets device id and name pairs."
+  (let ((index 0))
+    (remove-if #'null
+               (mapcar (lambda (p)
+                         (incf index)
+                         (if (equal state p)
+                             (nth (- index 2) arguments)
+                             nil))
+                       arguments))))
 
 (defun main (arguments)
   "The main entry point of the program."
   (let* ((choice (car arguments))
-         (xrandr-devices (cdr arguments))
-         (connected-devices (concatenate 'list
-                                         (mapcar (lambda (x)
-                                                   (car (string-to-list x #\ )))
-                                                 (remove-if-not (lambda (x)
-                                                                  (string-equal (cadr (string-to-list x #\ )) "connected"))
-                                                                xrandr-devices))))
+         (devices-info (cdr arguments))
+         (connected-devices (get-device-names devices-info "connected"))
          (primary-device (car connected-devices))
-         (disconnected-devices (concatenate 'list
-                                            (mapcar (lambda (x)
-                                                      (car (string-to-list x #\ )))
-                                                    (remove-if-not (lambda (x)
-                                                                     (string-equal (cadr (string-to-list x #\ )) "disconnected"))
-                                                                   xrandr-devices)))))
+         (disconnected-devices (get-device-names devices-info "disconnected")))
     (princ (cond ((string-equal choice "connected") connected-devices)
                  ((string-equal choice "disconnected") disconnected-devices)
                  (t primary-device)))))
