cc7362ca4a1a5a01469148946ca42df40b92db48
myTerminal <team.terminal@gmail.com>
2024-01-01 00:03:13 -0600

    make clipboard aliases generic

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -6,6 +6,6 @@ add-to-path $HOME/bin
 add-to-path $HOME/.local/bin
 
 # Set up aliases
-alias xcopy="xsel --clipboard --input"
-alias xpaste="xsel --clipboard --output"
+alias mt-copy="xsel --clipboard --input"
+alias mt-paste="xsel --clipboard --output"
 alias keymon-start="key-mon --key-timeout=1.5 --no-press-fadeout=2 --old-keys=5"
